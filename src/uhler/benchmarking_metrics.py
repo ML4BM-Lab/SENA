3cfc64b48c17420c76aa5f38d31294db11268077
Anonymous Candidate <anon@example.com>
2024-07-20 23:21:09 +0200

    include heatmap analysis of weights

--- src/uhler/benchmarking_metrics.py
+++ src/uhler/benchmarking_metrics.py
@@ -7,23 +7,56 @@ from matplotlib import cm,colors
 from warnings import filterwarnings
 from tqdm import tqdm
 import os
-
 filterwarnings('ignore')
 sys.path.append('./../src')
-
 import scanpy as sc
 sc.settings.verbosity = 3
 sc.settings.set_figure_params(dpi=80, facecolor='white', frameon=False)
-
 from scipy.stats import pearsonr
 from sklearn.metrics import r2_score
+import seaborn as sns
 import pandas as pd
-
 from inference import *
 from utils import SCDATA_sampler, MMD_loss
 from dataset import SCDataset
 from torch.utils.data import DataLoader
 
+def load_data_raw_go(ptb_targets):
+
+    #define url
+    datafile='./../../data/Norman2019_raw.h5ad'
+    adata = sc.read_h5ad(datafile)
+
+    # load gos from NA paper
+    GO_to_ensembl_id_assignment = pd.read_csv(os.path.join('..','..','data','delta_selected_pathways','go_kegg_gene_map.tsv'),sep='\t')
+    GO_to_ensembl_id_assignment.columns = ['GO_id','ensembl_id']
+
+    #load GOs
+    go_2_z_raw = pd.read_csv(os.path.join('..','..','data','topGO_Jesus.tsv'),sep='\t')
+    GO_to_ensembl_id_assignment = GO_to_ensembl_id_assignment[GO_to_ensembl_id_assignment['GO_id'].isin(go_2_z_raw['PathwayID'].values)]
+
+    ## load interventions
+    ensembl_genename_mapping = pd.read_csv(os.path.join('..','..','data','delta_selected_pathways','ensembl_genename_mapping.tsv'),sep='\t')
+    ensembl_genename_mapping = dict(zip(ensembl_genename_mapping.iloc[:,0], ensembl_genename_mapping.iloc[:,1]))
+    intervention_genenames = map(lambda x: ensembl_genename_mapping.get(x,None), GO_to_ensembl_id_assignment['ensembl_id'])
+    intervention_to_GO_assignment_genes = list(set(intervention_genenames).intersection(set([x for x in adata.obs['guide_ids'] if x != '' and ',' not in x])))
+        
+    assert all(np.array(sorted(ptb_targets)) == np.array(sorted(intervention_to_GO_assignment_genes)))
+    
+    # ## keep only GO_genes
+    adata = adata[:, adata.var_names.isin(GO_to_ensembl_id_assignment['ensembl_id'])]
+    adata.obs = adata.obs.reset_index(drop=True)
+
+    ## get control
+    perturbations_idx_dict = {}
+    for gene in ['ctrl'] + ptb_targets:
+
+        if gene == 'ctrl':
+            perturbations_idx_dict[gene] = (adata.obs[adata.obs['guide_ids'] == '']).index.values
+        else:
+            perturbations_idx_dict[gene] = (adata.obs[adata.obs['guide_ids'] == gene]).index.values
+
+    return adata, perturbations_idx_dict, sorted(set(go_2_z_raw['PathwayID'].values)), sorted(set(go_2_z_raw['topGO'].values)) 
 
 def compute_metrics(model_name = 'gosize5'):
 	
@@ -133,34 +166,66 @@ def visualize_gradients(model_name = 'gosize5'):
     with open(f'{savedir}/ptb_targets.pkl', 'rb') as f:
         ptb_targets = pickle.load(f)
 
-    def plot_layer_weights(layer_name, model):
+    fpath = os.path.join('./../../','figures','uhler_paper',model_name)
+    if not os.path.isdir(fpath):
+        os.mkdir(fpath)
+
+    def plot_layer_weights(layer_name, model, fpath):
 
         ## get non-zero gradients
-        try:
-            non_masked_gradients = eval(f'model.{layer_name}.weight[(model.{layer_name}.weight * model.{layer_name}.mask.T) != 0].detach().cpu().numpy()')
-            masked_gradients = eval(f'model.{layer_name}.weight[(model.{layer_name}.weight * model.{layer_name}.mask.T) == 0].detach().cpu().numpy()')
-        except:
-            non_masked_gradients = eval(f'model.{layer_name}.weight[model.{layer_name}.weight != 0].detach().cpu().numpy()')
-            masked_gradients = eval(f'model.{layer_name}.weight[model.{layer_name}.weight == 0].detach().cpu().numpy()')
-
+        non_masked_gradients = eval(f'model.{layer_name}.weight[(model.{layer_name}.weight * model.{layer_name}.mask.T) != 0].detach().cpu().numpy()')
+        masked_gradients = eval(f'model.{layer_name}.weight[(model.{layer_name}.weight * model.{layer_name}.mask.T) == 0].detach().cpu().numpy()')
 
         ## Plotting the histogram
         plt.figure(figsize=(10, 6))
         plt.hist(non_masked_gradients, bins=30, alpha=0.5, label='Non-masked values')
         plt.hist(masked_gradients, bins=30, alpha=0.5, label='Masked values')
+        plt.yscale('log')
         plt.legend()
         plt.xlabel('Value')
         plt.ylabel('Frequency')
         plt.title(f'Layer {layer_name} weights')
-        plt.show()
-        plt.savefig(os.path.join('./../../','result', model_name, f'{model_name}_layer_{layer_name}_histplot.png'))
+        plt.savefig(os.path.join(fpath, f'{model_name}_layer_{layer_name}_histplot.png'))
 
-    plot_layer_weights(layer_name='fc1', model=model)
-    plot_layer_weights(layer_name='fc_mean', model=model)
-    plot_layer_weights(layer_name='fc_var', model=model)
+    def plot_weight_heatmap(layer_name, model, fpath):
+
+        ##get the output of NetActivity Layer
+        #batch_size, mode = 128, 'train'
+        #dataloader, _, _, _, ptb_targets = get_data(batch_size=batch_size, mode=mode)
+        adata, _, gos, zs = load_data_raw_go(ptb_targets)
+
+        weight_mat = eval(f'model.{layer_name}.weight.detach().cpu().numpy()').T
+        weight_df = pd.DataFrame(weight_mat)
+
+        if layer_name == 'fc1':
+
+            weight_df.index = adata.var['gene_symbols']
+            weight_df.columns = gos
+           
+        elif layer_name == 'fc_mean' or layer_name == 'fc_var':# or layer_name == 'z':
+
+            weight_df.index = gos
+            weight_df.columns = zs
+
+        ## Plotting the histogram
+        plt.figure(figsize=(25, 150))
+        sns.heatmap(weight_df.abs(), cmap='coolwarm', center=0, annot=False, linewidths=.5)
+        plt.title(f'Heatmap of Gene Weights - Layer {layer_name}')
+        plt.xlabel('GO Terms')
+        plt.ylabel('Gene Symbols')
+        plt.savefig(os.path.join(fpath, f'{model_name}_layer_{layer_name}_heatmap.png'))
+        
+    ## hist
+    plot_layer_weights(layer_name='fc1', model=model, fpath=fpath)
+    plot_layer_weights(layer_name='fc_mean', model=model, fpath=fpath)
+    plot_layer_weights(layer_name='fc_var', model=model, fpath=fpath)
+
+    ## heatmap
+    plot_weight_heatmap(layer_name='fc_mean', model=model, fpath=fpath)
+    plot_weight_heatmap(layer_name='fc_var', model=model, fpath=fpath)
 
 ## model name
-model_name = 'full_go_NA+deltas'
+model_name = 'raw_go_NA_NA'
 
 ## compute metrics
 compute_metrics(model_name = model_name)
