4509bb88b148f7a556c2e71fb10b8776d065626c
Jan Voges <janvoges.git@icloud.com>
2024-11-24 23:38:07 +0100

    GEARS running on margaret (device="cpu")

--- gears/install_gears.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-# pip3 install torch_geometric
-pip3 install torch_geometric -f https://data.pyg.org/whl/torch-2.5.0+cpu.html
-pip3 install cell-gears
