2ad94dd572be08ee3bc622594977dae1799ddd0e
Jan Voges <janvoges.git@icloud.com>
2024-11-26 09:57:42 +0100

    Remove devcontainer

--- .devcontainer/post_create.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Remove all editor settings for Python from the VS Code remote settings.
-sed --in-place '/"\[python\]": {/,/}/d' /home/vscode/.vscode-server/data/Machine/settings.json
-
-# Install additional packages.
-sudo apt-get update && sudo apt-get install --yes shellcheck
